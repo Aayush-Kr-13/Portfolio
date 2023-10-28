841933cc1a78ef678e77eb5c65f04632fcbbb3be
Aayush Kumar <aayush@Aayushs-MacBook-Air.local>
2023-10-28 20:35:54 +0530

    completed

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo';
+import Loader from 'react-loaders';
 
 
 const Home = () => {
@@ -25,6 +26,7 @@ const Home = () => {
       
 
     return (
+      <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -51,6 +53,8 @@ const Home = () => {
             </div>
             <Logo />
         </div>
+        <Loader type="pacman" />
+      </>
     )
 }
 
